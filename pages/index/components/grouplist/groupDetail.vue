7180de9a8d4eff3fb125387fea75dc5fbb142a40
e <chenyankun95@126.com>
2019-07-02 23:02:15 +0800

    home05

--- pages/index/components/grouplist/groupDetail.vue
+++ pages/index/components/grouplist/groupDetail.vue
@@ -1,22 +1,82 @@
 <template>
 	<view class="groupD">
 		<view class="header">
-			<view class=""></view>
+			<view class="backgroup"></view>
+			<view class="photo">
+				<image src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3000033855,214344624&fm=26&gp=0.jpg"></image>
+			</view>
+			<view class="title">
+				<view>龙骑士</view>
+				<view style="font-size: 14px;">签名：不服就干</view>
+			</view>
 		</view>
+		
+		<view class="body">
+			<view class="groupMsg">
+				<view>信息</view>
+				<view class="context">
+					<view class="item">
+						时间：2019
+					</view>
+				</view>
+			</view>
+		</view>
+		
 		
 	</view>
 </template>
 
 <script>
+	import {uniTag } from '@dcloudio/uni-ui'
+	export default {
+		components: {
+			uniTag 
+		},
+		data(){
+			return {
+				
+			}
+		}
+	}
 </script>
 
 <style>
 	.header{
 		width: 100%;
 		height: 240upx;
+		position: relative;
+	}
+	.backgroup{
+		height: 100%;
 		background-image: url("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561467102487&di=51a2165e4319ce957afd6b2ba0d90d99&imgtype=0&src=http%3A%2F%2Fpic31.nipic.com%2F20130705%2F9527735_231540074000_2.jpg");
 		background-size:cover;
 		opacity: 0.5;
-		position: relative;
+	}
+	.header .photo {
+		position: absolute;
+		width: 200upx;
+		height: 200upx;
+		left: 100upx;
+		top: 140upx;
+		border: 20upx solid #dddd;
+		border-radius: 200upx;
+		overflow: hidden;
+	}
+	.photo image {
+		width: 100%;
+		height: 100%;
+	}
+	.title {
+		position: absolute;
+		height: 100upx;
+		line-height: 60upx;
+		left: 380upx;
+	}
+	.tag {
+		position: absolute;
+	}
+	.body {
+		padding: 20upx;
+		padding-top: 180upx;
 	}
 </style>
