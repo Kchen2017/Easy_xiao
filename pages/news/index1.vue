03e7d636bdf09119bb1359b0f87262d04a3f9922
陈砚坤 <chenyankun@ZBMAC-C02WD3Q-5.local>
2019-08-15 18:42:27 +0800

    jd40

---
+++ pages/news/index1.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<dongtai></dongtai>
+	</view>
+</template>
+
+<script>
+	import dongtai from "../index/components/dongtaiCom.vue"
+	export default {
+		components: {
+			dongtai
+		},
+		data(){
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
